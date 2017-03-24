a68ed800372a1ee396392e3cb1073798972a1aa9
Ilene Gorski <ilene.gorski@gmail.com>
2017-03-24 09:48:19 -0700

    Add working alerts for back-end options

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ $(function() {
       alert("Please choose front-end or back-end");
     }
     else if (whichEndInput === "front") {
+      $(".layer2").hide();
       $("#front-end").show();
       $("#css-or-design").change(function() {
         var cssDesignInput = $("#css-or-design").val();
@@ -18,6 +19,22 @@ $(function() {
         else if (cssDesignInput === "design") {
           $(".outcome").hide();
           $("#designCourse").show();
+        }
+      });
+    }
+    else if (whichEndInput === "back") {
+      $("layer2").hide();
+      $("#back-end").show();
+      $("#large-or-small").change(function() {
+        var largeOrSmallInput = $("#large-or-small").val();
+        if (largeOrSmallInput === "choose") {
+          alert("Please choose what size company to work for")
+        }
+        else if (largeOrSmallInput === "large") {
+          alert("You will learn C# or Java")
+        }
+        else if (largeOrSmallInput === "small") {
+          alert("You will learn PHP or Ruby")
         }
       });
     }
