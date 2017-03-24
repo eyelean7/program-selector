006ecd4e39c3121b8ab62fec35036675b35daaa3
Ilene Gorski <ilene.gorski@gmail.com>
2017-03-24 09:25:52 -0700

    Add first layers of selection bars

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(function() {
+  $("#whichEndSelect").change(function() {
+    var whichEndInput = $("#whichEndSelect").val();
+    if (whichEndInput === "choose") {
+      alert("Please choose front-end or back-end");
+    }
+    else if (whichEndInput === "front") {
+      $("#front-end").show();
+      $("#css-or-design").change(function() {
+        var cssDesignInput = $("#css-or-design").val();
+        if (cssDesignInput === "choose") {
+          alert("Please choose the more appealing option")
+        }
+      });
+    }
+  });
+});
